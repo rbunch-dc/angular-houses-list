34403ef93229dc9c0312a09e56b2f1290b877821
Robert Bunch <rob@digitalcrafts.com>
2016-03-29 11:49:31 -0400

    Add houses

--- controller.js
+++ controller.js
@@ -1,5 +1,5 @@
 
-    angular.module('houseList', []).controller('housesListCtrl', housesListCtrl);
+    angular.module('houseList1', []).controller('housesListCtrl', housesListCtrl);
 
     function housesListCtrl($scope) {
 
@@ -17,5 +17,84 @@
 
     }
 
+angular.module('houseList', []).controller('siteEditCtrl', siteEditCtrl);
 
- 
+	function siteEditCtrl($scope) {
+	  $scope.houses = houses;
+	  $scope.startAdd = startAdd;
+	  $scope.cancel = cancel;
+	  $scope.add = add;
+	  $scope.startEdit = startEdit;
+	  $scope.save = save;
+	  $scope.startRemove = startRemove;
+	  $scope.remove = remove;
+	  $scope.getSelected = getSelected;
+
+	  var selected = -1;
+	  setView('list');
+
+	  function setView(view) {
+	    $scope.view = view;
+	  }
+
+	  function startAdd() {
+	    $scope.houseBox = '';
+	    setView('add');
+	  }
+
+	  function cancel() {
+	    setView('list');
+	  }
+
+	  function add() {
+	    $scope.houses.push({
+	      name: $scope.houseBox
+	    });
+	    setView('list');
+	  }
+
+	  function startEdit(index) {
+	    selected = index;
+	    $scope.houseBox = $scope.houses[index];
+	    setView('edit');
+	  }
+
+	  function save() {
+	    $scope.houses[selected] = $scope.houseBox;
+	    setView('list');
+	  }
+
+	  function startRemove(index) {
+	    selected = index;
+	    setView('delete');
+	  }
+
+	  function remove() {
+	    $scope.houses.splice(selected, 1);
+	    setView('list');
+	  }
+
+	  function getSelected() {
+	    return houses[selected].name;
+	  }
+}
+
+angular.module('houseList2', ['ngRoute'])
+  .controller('adminCtrl', AdminCtrl)
+  .config(function ($routeProvider) {
+    $routeProvider.when('/locations', {
+    	// alert("hello!!");
+      templateUrl: 'views/listHouses.html'
+    });
+    $routeProvider.when('/sites', {
+      templateUrl: 'views/sites.html'
+    });
+    $routeProvider.otherwise({
+      templateUrl: 'views/main.html'
+    });
+  });
+
+function AdminCtrl($scope) {
+	$scope.view = 'list';
+
+}
